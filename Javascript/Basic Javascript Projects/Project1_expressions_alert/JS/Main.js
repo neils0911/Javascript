94e1fbfc44c18ba456855d2d0d2ecb811fac8894
neils0911 <101988267+neils0911@users.noreply.github.com>
2022-07-19 19:17:04 +0100

    Add files via upload

---
+++ Javascript/Basic Javascript Projects/Project1_expressions_alert/JS/Main.js
@@ -0,0 +1,14 @@
+function My_First_Function() { //Defining a function and naming it
+    var string = "This text is green!"; //Defining a variable and giving it a
+    var result = string.fontcolor("green"); //string value
+    document.getElementById("Green_Text").innerHTML = result; //Putting the value
+                                        //of result into HTML element with "Green_Text" id
+                                        
+
+}
+
+var Family = "The Arezzinis", Dad = "Jerimiah", Mom = "Hermoine", //Variables
+		Daughter = "Penny", Son = "Zorro";
+document.write(Dad); //Result
+
+window.alert("Example!")//Pop up window with message
