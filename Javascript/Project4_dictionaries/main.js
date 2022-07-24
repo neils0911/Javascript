fcca27f74330cbcd38533a68f24a7e4df85d9047
neils0911 <101988267+neils0911@users.noreply.github.com>
2022-07-24 18:22:07 +0100

    Add files via upload

---
+++ Javascript/Project4_dictionaries/main.js
@@ -0,0 +1,11 @@
+function Dog_Dictionary() {
+    var Animal = {
+        Species:"Dog",
+        Color:"Black",
+        Breed:"Labrador",
+        Age: 5,
+        Sound:"Bark!"
+    };
+    delete Animal.Sound;
+    document.getElementById("Dictionary").innerHTML = Animal.Sound;
+}
